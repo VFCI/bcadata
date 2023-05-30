4fdce29538f4ad7279a4eb84a892718c71edb9cd
matdehaven <matthew_dehaven@brown.edu>
2023-05-30 15:07:24 -0400

    Removing temp file

--- temp.R
+++
@@ -1,7 +0,0 @@
-install.packages("devtools")
-require(devtools)
-create_package("../bcadata")
-
-(x <- "alfa,bravo,charlie,delta")
-strsplit1(x, split = ",")
-exists("strsplit1", where = globalenv(), inherits = FALSE)
