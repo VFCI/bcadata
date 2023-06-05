0be26fccddada09ebf6466a5a79a34c030aa8c0d
matdehaven <matthew_dehaven@brown.edu>
2023-06-05 19:43:59 -0400

    Added use of ALFRED for vintages

--- R/pull_fred_data.R
+++ R/pull_fred_data.R
@@ -1,10 +1,12 @@
 #' Pull data from FRED
 #'
 #' @param fred_api_key API key from FRED
+#' @param vintage Boolean, use original paper vintage
 #'
 #' @return A tibble/data.frame
 #'
-pull_fred_data <- function(fred_api_key) {
+pull_fred_data <- function(fred_api_key, vintage = F) {
+  qdate <- value <- NULL
   fredr::fredr_set_key(fred_api_key)
 
   ## Fred series codes that are pulled
@@ -31,10 +33,61 @@ pull_fred_data <- function(fred_api_key) {
     fredr::fredr(series, frequency = frequency, aggregation_method = agg)
   }
 
-  data <- Reduce(rbind, lapply(
-    series, my_fredr,
-    frequency = "q"
-  ))
+  my_alfredr <- function(series) {
+
+    df <- alfred::get_alfred_series(series, "value",
+        realtime_start = "2018-05-01", realtime_end = "2018-05-01") |>
+        dplyr::as_tibble() |>
+        dplyr::select(date, value)
+
+    df <- df |>
+        dplyr::mutate(qdate = lubridate::date(paste0(
+            lubridate::year(date), "-", lubridate::quarter(date) * 3 - 2, "-01"
+        )))
+
+    if(series == "CE16OV") {
+        df <- df |> 
+            dplyr::group_by(qdate) |>
+            dplyr::summarize(value = mean(value)) |>
+            dplyr::mutate(date = qdate) |>
+            dplyr::ungroup()
+
+    }
+    if(series == "UNRATE") {
+        df <- df |>
+            dplyr::group_by(qdate) |>
+            dplyr::filter(date == max(date)) |>
+            dplyr::mutate(date = qdate) |>
+            dplyr::ungroup()
+    }
+    if(series == "FEDFUNDS") {
+        df <- df |>
+            dplyr::group_by(qdate) |>
+            dplyr::filter(date == max(date)) |>
+            dplyr::mutate(date = qdate) |>
+            dplyr::ungroup()
+    }
+    if(series == "CNP16OV") {
+        df <- df |>
+            dplyr::group_by(qdate) |>
+            dplyr::filter(date == max(date)) |>
+            dplyr::mutate(date = qdate) |>
+            dplyr::ungroup()
+    }
+
+    df <- df |> dplyr::select(date, value) |>
+        tidyr::pivot_longer(cols = value, names_to = "series_id") |>
+        dplyr::mutate(series_id = series)
+
+    return(df)
+}
+
+  if(vintage == T) {
+    data <- Reduce(rbind, lapply(series, my_alfredr))
+  }else{
+    data <- Reduce(rbind, lapply(
+      series, my_fredr, frequency = "q"))
+  }
 
   data <- data[, c("date", "series_id", "value")]
 
